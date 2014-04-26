7d0744f75c2b55a1be4f27a621f2d3a2236953bc
Sira Cuervo <fl.siracusa@outlook.com.ar>
2014-04-26 17:31:55 -0300

    Build AxxionControl in settings package 2/2

--- Android.mk
+++ Android.mk
@@ -1,17 +1,24 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_JAVA_LIBRARIES := bouncycastle telephony-common
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13 android-support-v4
-
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
-
-LOCAL_PACKAGE_NAME := AxxionControl
-LOCAL_CERTIFICATE := platform
-
-include $(BUILD_PACKAGE)
-
+#LOCAL_PATH:= $(call my-dir)
+#include $(CLEAR_VARS)
+#
+#LOCAL_JAVA_LIBRARIES := bouncycastle telephony-common
+#LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13 android-support-v4
+#
+#LOCAL_JAVA_LIBRARIES += org.cyanogenmod.hardware
+#
+#LOCAL_MODULE_TAGS := optional
+#
+#LOCAL_SRC_FILES := $(call all-java-files-under, src)
+#
+#LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res
+#
+#LOCAL_AAPT_FLAGS := --auto-add-overlay
+#
+#LOCAL_PACKAGE_NAME := AxxxionControl
+#LOCAL_CERTIFICATE := platform
+#LOCAL_PRIVIELEGED_MODULE := true
+#
+#include $(BUILD_PACKAGE)
+#
 # Use the folloing include to make our test apk.
-include $(call all-makefiles-under,$(LOCAL_PATH))
+#include $(call all-makefiles-under,$(LOCAL_PATH))
